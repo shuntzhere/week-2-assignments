5321619debaa54b8c430629b7e6a1b170ea0b254
Masoom Kumar <masoom.kr04@gmail.com>
2022-01-29 16:04:14 +0530

    refactored

--- header/short.js
+++ header/short.js
@@ -1,6 +1,9 @@
 const shrinkNav = document.querySelector(".short-box");
-
+console.log("shivansh");
 const shrink = () => {
   console.log(this.scrollY);
+  if (this.scrollY) {
+    shrinkNav.classList.add(".nav__short");
+  }
 };
-shrinkNav.addEventListener("click", shrink);
+shrinkNav.addEventListener("scroll", shrink);
